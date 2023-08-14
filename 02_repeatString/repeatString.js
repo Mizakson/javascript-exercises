91698c6e1d790f4f247c7735d83e8ee5392f1df5
Mizakson <maxwellizakson@gmail.com>
2023-08-14 12:16:32 -0400

    Add error statement, remove .skip from all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,6 +2,9 @@ const repeatString = function(str, num) {
     for (let i = 0; i <= num; i++) {
         return str.repeat(num);
     }
+    if (num < 0) {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
