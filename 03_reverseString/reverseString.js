f52d9fb63b7fbcd501405a7aa8e89805b1d1d539
Mizakson <maxwellizakson@gmail.com>
2023-08-16 10:39:34 -0400

    Add reverserString function to .js file
    split, reverse, join

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitStr = str.split("");
+    let reverseArr = splitStr.reverse();
+    let finalArr = reverseArr.join("");
+    return finalArr;
 };
 
 // Do not edit below this line
