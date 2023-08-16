8836212c2b3a4aa03c333b5de70cea129055690a
Mizakson <maxwellizakson@gmail.com>
2023-08-16 12:21:21 -0400

    Tweak body of sumAll function in .js file
    difficulty connecting both conditions
    successfully passed the first three tests, but when
    I ran all tests only the last three passed.
    
    looked at solution to understand the use of a temp variable
    and if i should put the typeof statement and check if < 0 at the
    start of the function.
    
    Overall got around 90 % of the problem right, was just having trouble
    sorting out the return statements, the loop was written correctly
    and I used comparison operator ( || ) in the first two conditional
    statements.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
+    if (min < 0 || max < 0) return 'ERROR';
+    if (min > max) {
+        let swap = min;
+        max = swap;
+        min = max;
+    }
 
+    let finalSum = 0;
+    for (let i = 0; i <= max; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
+    
 };
 
 // Do not edit below this line
