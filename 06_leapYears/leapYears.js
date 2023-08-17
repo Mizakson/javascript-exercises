e6e592d245324cbc8f16d23b2ac9d46bca34d876
Mizakson <maxwellizakson@gmail.com>
2023-08-17 10:21:15 -0400

    Finish exercise 6
    if && conditional statement with boolean return value

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // leap years are divisible by four
+    // years divisible by 100 are not leap years
+    // unless they are divisible by 400
+    if (year % 100 === 0 && year % 400 !== 0 || year % 4 !== 0) {
+        return false;
+    } else {
+        return true;
+    }
+
 
 };
 
