6c867304573accf942959c3d7a8851c6177f17c8
Mizakson <maxwellizakson@gmail.com>
2023-08-17 11:23:38 -0400

    Complete both conversion functions in .js file

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
 // round to 1 decimal place
 // Math.round(number * 10) / 10;
 
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celciusTemp = (temp - 32) * (5/9);
+  let roundedTemp = Math.round(celciusTemp * 10) / 10;
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheitTemp = (temp * (9/5)) + 32;
+  let roundedTemp = Math.round(fahrenheitTemp * 10) / 10;
+  return roundedTemp;
+
 };
 
 // Do not edit below this line
