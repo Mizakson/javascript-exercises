c64cf3c607f6000364db0e125b4d51566a622144
Mizakson <maxwellizakson@gmail.com>
2023-09-01 09:18:19 -0400

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((a,b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	var result = num;
+  if (num === 0 | num === 1) {
+    return 1;
+  }
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line
