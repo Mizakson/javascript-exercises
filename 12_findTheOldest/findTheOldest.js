d214c444c6986466947c912846bfaf09b0fd94bb
Mizakson <maxwellizakson@gmail.com>
2023-09-05 11:37:42 -0400

    Complete exercise
    view part of solution
    
    could not return proper solution while
    chaining map and filter methods
    read mdn docs on reduce
    refer to array cardio videos for reference

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+
+    const findAge = function (birth, death) {
+        if (!death) {
+            death = new Date().getFullYear();
+        }
+        return death - birth;
+    };
+    
+    return arr.reduce((oldest, current) => {
+            const oldestAge = findAge(oldest.yearOfBirth, oldest.yearOfDeath);
+            const currentAge = findAge(current.yearOfBirth, current.yearOfDeath); 
+            return oldestAge < currentAge ? current : oldest;});
 
 };
 
