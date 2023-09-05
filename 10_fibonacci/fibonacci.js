433d07aa007ffa585ddef248f7e79fa2ad370cf9
Mizakson <maxwellizakson@gmail.com>
2023-09-05 10:36:56 -0400

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    Number(num);
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+    let i = 1;
+    for (i = 1; i < num; i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+    }
+    if (num < 0) {
+        return 'OOPS';
+    }
+    return num2;
 };
 
 // Do not edit below this line
